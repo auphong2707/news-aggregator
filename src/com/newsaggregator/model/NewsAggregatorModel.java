d993d6e4d22b41472f66a12471e8a304fb627906
Veil <65228307+auphong2707@users.noreply.github.com>
2024-03-20 13:05:56 +0700

    Test update

--- src/com/newsaggregator/model/NewsAggregatorModel.java
+++ src/com/newsaggregator/model/NewsAggregatorModel.java
@@ -3,9 +3,6 @@ package com.newsaggregator.model;
 public class NewsAggregatorModel {
 	public static void main(String args[]) {
 		WebScrapperFT scraper = new WebScrapperFT();
-		ArticleData test = scraper.scrapeArticle("https://www.ft.com/content/2daf0c89-a2b5-4828-bb76-18b1e5aa99ef");
-		for (DataType feature : DataType.values()) {
-			System.out.println(feature + " " + test.getDataByType(feature));
-		}
+		scraper.scrapeAllData();
 	}
 }
--- src/com/newsaggregator/model/Test.java
+++ src/com/newsaggregator/model/Test.java
@@ -1,16 +1,41 @@
 package com.newsaggregator.model;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Test {
 	public static void main(String[] args) {
-		List<String> arr1 = new ArrayList<String>();
-		List<String> arr2 = new ArrayList<String>();
-		arr1.add("phong");
-		arr2.add("nononon");
-		arr2.addAll(arr1);
-		System.out.println(arr2);
+		FileWriter fileWriter = null;
+		String fileName = "data/newsFT.csv";
+		List<String> listOfData = new ArrayList<String>();
+		listOfData.add("1234");
+		listOfData.add("4155");
 		
-		
+        try {
+            fileWriter = new FileWriter(fileName);
+ 
+            // Write the CSV file header
+            fileWriter.append(ArticleData.HEADER);
+ 
+            // Write a new ArticleData object list to the CSV file
+            for (String unit : listOfData) {
+            	fileWriter.append(unit);
+            }
+ 
+            System.out.println("CSV file was created successfully !!!");
+ 
+        } catch (Exception e) {
+            System.out.println("Error in CsvFileWriter !!!");
+            e.printStackTrace();
+        } finally {
+            try {
+                fileWriter.flush();
+                fileWriter.close();
+            } catch (IOException e) {
+                System.out.println("Error while flushing/closing fileWriter !!!");
+                e.printStackTrace();
+            }
+        }
 	}
 }
