eef88efd9b2d1707616b2a2581d35c4406788cfd
Veil <65228307+auphong2707@users.noreply.github.com>
2024-03-26 00:11:30 +0700

    Update Test.java

--- src/com/newsaggregator/model/Test.java
+++ src/com/newsaggregator/model/Test.java
@@ -1,11 +1,16 @@
 package com.newsaggregator.model;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
+
 import java.util.List;
 
 public class Test {
 	public static void main(String[] args) {
+		Model model = new Model();
 		
+		List<ArticleData> result = model.search("Facebook Libra: the");
+		
+		for(ArticleData article : result)
+		{
+			System.out.println(article.getDataByType(DataType.TITLE));
+		}
 	}
 }
