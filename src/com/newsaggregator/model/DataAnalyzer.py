2a2d85f8b1d05996d0053139e2f642dad8f7dfad
Veil <65228307+auphong2707@users.noreply.github.com>
2024-03-27 00:16:45 +0700

    Update DataAnalyzer.py
    Change directory to All

--- src/com/newsaggregator/model/DataAnalyzer.py
+++ src/com/newsaggregator/model/DataAnalyzer.py
@@ -8,7 +8,7 @@ CURRENT_WORKING_DIRECTORY = __file__.replace('\\', '/')\
                                     .replace('src/com/newsaggregator/model/DataAnalyzer.py', '')
 
 # Get the data
-data_file = open(CURRENT_WORKING_DIRECTORY + 'data/newsFT.json', encoding = "utf8")
+data_file = open(CURRENT_WORKING_DIRECTORY + 'data/newsAll.json', encoding = "utf8")
 data = json.load(data_file)
 
 # Loading data into the engine
