1072df9bacb466d08e53a06013115c2e96607605
Veil <65228307+auphong2707@users.noreply.github.com>
2024-03-26 22:19:50 +0700

    Delete NewsAggregatorModel.java

--- src/com/newsaggregator/model/NewsAggregatorModel.java
+++
@@ -1,9 +0,0 @@
-package com.newsaggregator.model;
-
-public class NewsAggregatorModel {
-	public static void main(String args[]) {
-		// WebScrapperFT scraperFT = new WebScrapperFT();
-		WebScrapperCONV scraperCONV = new WebScrapperCONV();
-		scraperCONV.scrapeAllData();
-	}
-}
