cefba582ec8131b7abe3f4a71629eb70cc89b6b3
Veil <65228307+auphong2707@users.noreply.github.com>
2024-03-26 22:12:08 +0700

    Update Test.java

--- src/com/newsaggregator/model/Test.java
+++ src/com/newsaggregator/model/Test.java
@@ -6,15 +6,6 @@ import java.util.List;
 public class Test {
 	public static void main(String[] args) throws IOException, InterruptedException{
 		Model model = new Model();
-		model.runLocalServer();
-		
-		List<ArticleData> result = model.search("Facebook Libra: the");
-		
-		for(ArticleData article : result)
-		{
-			System.out.println(article.getDataByType(DataType.TITLE));
-		}
-		
-		model.terminateLocalServer();
+		model.combineData();
 	}
 }
