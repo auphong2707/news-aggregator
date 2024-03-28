51cb98d4cf37aee9e8e10115a0bc9085e75aad17
whatsoever025 <149892454+whatsoever025@users.noreply.github.com>
2024-03-28 12:54:11 +0700

    Delete JavaFxTutirial directory

--- JavaFxTutirial/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module JavaFxTutirial {
-	requires javafx.controls;
-	requires javafx.graphics;
-	requires javafx.base;
-	requires javafx.fxml;
-	requires java.desktop;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
