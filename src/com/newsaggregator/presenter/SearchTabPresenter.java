4368165748832e01e40a0cc3d9bb86093fec94fa
whatsoever025 <149892454+whatsoever025@users.noreply.github.com>
2024-03-30 14:19:35 +0700

    search tab update

--- src/com/newsaggregator/presenter/SearchTabPresenter.java
+++ src/com/newsaggregator/presenter/SearchTabPresenter.java
@@ -3,15 +3,24 @@ package com.newsaggregator.presenter;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class SearchTabPresenter {
 	@FXML
+	private Button searchButton;
+	@FXML
+	private TextField searchBar;
+	@FXML
 	private Label category1;
-	
-	public void category(ActionEvent event) {
-		
+	public void category(MouseEvent event) {
+		System.out.print("Article");
 	}
-	
+	public void searchbar(ActionEvent event) {
+		searchButton.setVisible(false);
+		searchBar.setVisible(true);
+	}
 	
 }
