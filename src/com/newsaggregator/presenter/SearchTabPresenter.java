e809e4c1985bb28a582d20dbf82b338eda56ed99
whatsoever025 <149892454+whatsoever025@users.noreply.github.com>
2024-03-30 09:58:00 +0700

    Create SearchTabPresenter.java

---
+++ src/com/newsaggregator/presenter/SearchTabPresenter.java
@@ -0,0 +1,17 @@
+package com.newsaggregator.presenter;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class SearchTabPresenter {
+	@FXML
+	private Label category1;
+	
+	public void category(ActionEvent event) {
+		
+	}
+	
+	
+}
