ef830261ae42a1a27377052ab52dfeee27592dbc
TheSyx <minhquantsy@gmail.com>
2024-03-30 11:48:47 +0700

    Fix JavaFX

--- src/com/newsaggregator/presenter/HomepagePresenter.java
+++ src/com/newsaggregator/presenter/HomepagePresenter.java
@@ -1,5 +1,10 @@
 package com.newsaggregator.presenter;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class HomepagePresenter {
-	
+	@FXML
+	private Label date;
 }
---
+++ src/com/newsaggregator/presenter/Test.java
@@ -0,0 +1,36 @@
+package com.newsaggregator.presenter;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.fxml.*;
+
+public class Test extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // TODO  Auto-generated method stub
+
+
+    try {
+        Parent root = FXMLLoader.load(getClass().getResource("homepage.fxml"));
+        Scene scene = new Scene(root);
+
+        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.setMaximized(true);
+        primaryStage.show();
+//    	System.out.println(getClass().getResource("homepage.fxml"));
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
+}
