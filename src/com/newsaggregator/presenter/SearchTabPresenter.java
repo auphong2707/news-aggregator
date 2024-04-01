505bfac7c7a69f44aaf0b6d2c63258e35760a737
whatsoever025 <149892454+whatsoever025@users.noreply.github.com>
2024-04-01 23:20:32 +0700

    switch page update

--- src/com/newsaggregator/presenter/SearchTabPresenter.java
+++ src/com/newsaggregator/presenter/SearchTabPresenter.java
@@ -24,13 +24,18 @@ public class SearchTabPresenter {
 	private Model model = new Model();
 	
 	@FXML private Label dateLabel;
-	@FXML private Button searchButton;
+	@FXML private Label pageLabel;
+	@FXML private Button nextPage;
+	@FXML private Button previousPage;
 	@FXML private TextField searchBar;
 	@FXML private Group article1;
 	@FXML private Group article2;
 	@FXML private Group article3;
 	@FXML private Group article4;
 	@FXML private Group article5;
+	private List<ArticleData> searchData;
+	private int page = 1;
+	private List<Group> article = Arrays.asList(article1, article2, article3, article4, article5);
 
 	@FXML
 	void initialize() throws IOException, InterruptedException {
@@ -51,17 +56,33 @@ public class SearchTabPresenter {
 		model.terminateLocalServer();
 	}
 	
-	
-	
+	public void switchPage(ActionEvent event){
+		if (event.getSource() == nextPage && page <= 9 && page >= 1) {
+			page++;
+			pageLabel.setText("Page " + page);
+			switchArticle();
+		}else if (event.getSource() == previousPage && page >= 2 && page <= 10) {
+			page--;
+			pageLabel.setText("Page " + page);
+			switchArticle();
+		}
+	} 
+	private void switchArticle() {
+		for (int i=0; i < 5; i++)
+		{
+			PresenterTools.setArticleView(article.get(i), searchData.get(i+(page-1)*5), ArticleSize.BIG);
+		}
+	}
 	public void search(KeyEvent key) {
 		if (key.getCode() == KeyCode.ENTER) {
-			List<ArticleData> searchData = model.search(searchBar.getText());
-			List<Group> article = Arrays.asList(article1, article2, article3, article4, article5);
+			searchData = model.search(searchBar.getText());
+			page = 1;
+			pageLabel.setText("Page " + page);
 			for (int i=0; i < 5; i++)
 			{
-				PresenterTools.setArticleView(article.get(i), searchData.get(i), ArticleSize.BIG);
+				PresenterTools.setArticleView(article.get(i), searchData.get(i+(page-1)*5), ArticleSize.BIG);
 			}
-		}
+		} 
 	}
 	
 }
