0fa80c546369409e60cea412b68a7eaa9ca4cd74
dvtiendat <111187020+dvtiendat@users.noreply.github.com>
2024-04-03 17:04:44 +0700

    WebScrapper image links updated

--- src/com/newsaggregator/model/NewsAggregatorModel.java
+++ src/com/newsaggregator/model/NewsAggregatorModel.java
@@ -2,8 +2,8 @@ package com.newsaggregator.model;
 
 public class NewsAggregatorModel {
 	public static void main(String args[]) {
-		// WebScrapperFT scraperFT = new WebScrapperFT();
-		WebScrapperCONV scraperCONV = new WebScrapperCONV();
-		scraperCONV.scrapeAllData();
+		WebScrapperFT scraperFT = new WebScrapperFT();
+		// WebScrapperCONV scraperCONV = new WebScrapperCONV();
+		scraperFT.scrapeAllData();
 	}
 }
