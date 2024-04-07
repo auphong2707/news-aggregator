0e6b1e68b140b8bee7f90c399d85b327109687d9
Veil <65228307+auphong2707@users.noreply.github.com>
2024-04-07 22:55:53 +0700

    Update Test.java

--- src/com/newsaggregator/model/Test.java
+++ src/com/newsaggregator/model/Test.java
@@ -12,12 +12,6 @@ import org.jsoup.select.Elements;
 public class Test {
 	public static void main(String[] args) throws IOException, InterruptedException{
 		Model model = new Model();
-		model.runLocalServer();
-		WebScrapper scraper = new WebScrapperCONV();
-		scraper.scrapeAllData();
-		
-		model.terminateLocalServer();
-	
-
+		model.scrapeNewData();
 	}
 }
