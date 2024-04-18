089f52b25671df8c829e54383a9f5707f80c773c
King17pvp <tuanhai1304@gmail.com>
2024-04-18 20:16:11 +0700

    Modify DataCleaner.py #1

--- src/com/newsaggregator/model/DataCleaner.py
+++ src/com/newsaggregator/model/DataCleaner.py
@@ -17,15 +17,26 @@ class DataCleaner():
         self.data = []
     
     def import_data(self, filepath: str):
+        '''
+        Import data into the Summarizer via json file.
+        '''
+        
         f = open(filepath + 'data/newsAll.json', encoding = "utf8")
         self.data = json.load(f)
         
     def clean_data(self):
+        '''
+        Remove punctuations, unnecessary spaces, common stopwords from the title
+        and detailed content of every article in newsAll.json
+        '''
         for i in range(len(self.data)):
             self.data[i]['PROCESSED_TITLE'] = preprocessing(self.data[i]['TITLE'])
             self.data[i]['DETAILED_CONTENT_PROCESSED'] = preprocessing(self.data[i]['DETAILED_CONTENT'])
     
     def export_data(self, filepath: str):
+        '''
+        Export data to destined file under json format
+        '''
         with open(filepath + 'data/newsAllProcessed.json', 'w') as f:
             json.dump(self.data, f)
 
@@ -36,6 +47,10 @@ def remove_stop_words(text: str) -> str:
     return ' '.join(word for word in text.split() if word not in STOPWORDS)
 
 def preprocessing(str_input) -> str:
+    '''
+    Convert non-ascii characters to ascii ones, remove punctuations, redundant spaces
+    Return: a processed string
+    '''
     str_ascii = unidecode(str_input)
     translation_table = str.maketrans(string.punctuation, ' ' * len(string.punctuation))
     str_input_no_punct = str_ascii.translate(translation_table)
