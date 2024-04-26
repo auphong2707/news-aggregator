f849cb61bf1dbf03a5cc39b39239dac4c8e0ecfd
Veil <65228307+auphong2707@users.noreply.github.com>
2024-04-26 10:18:25 +0700

    Update WebScrapperFT.java

--- src/com/newsaggregator/model/WebScrapperFT.java
+++ src/com/newsaggregator/model/WebScrapperFT.java
@@ -15,7 +15,7 @@ class WebScrapperFT extends WebScrapper {
     {
     	webSource = "Financial Times";
     	type = "News Article";
-    	htmlContentLocation = ".article-body";
+    	htmlContentLocation = ".n-content-body js-article__content-body";
     	fileName += "newsFT.json";
     }
     
