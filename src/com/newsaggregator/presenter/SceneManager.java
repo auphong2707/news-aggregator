5065c162d5c83be00972149c8ce3d2442f526548
whatsoever025 <149892454+whatsoever025@users.noreply.github.com>
2024-05-05 09:14:32 +0700

    Update SceneManager.java

--- src/com/newsaggregator/presenter/SceneManager.java
+++ src/com/newsaggregator/presenter/SceneManager.java
@@ -1,25 +1,16 @@
 package com.newsaggregator.presenter;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 import com.newsaggregator.model.ArticleData;
 
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Cursor;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.shape.Line;
-import javafx.scene.text.Font;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 import javafx.util.Pair;
 
 public class SceneManager {
@@ -40,27 +31,21 @@ public class SceneManager {
 	private Scene[] scenes;
 	private Presenter[] presenters;
 	private SceneType currentSceneType;
-	
-	private Stage historyWindow = new Stage();
-	private VBox box = new VBox();
 
 	Stack<Pair<SceneType, Object>> backHistory = new Stack<Pair<SceneType, Object>>();
 	Stack<Pair<SceneType, Object>> forwardHistory = new Stack<Pair<SceneType, Object>>();
-	Stack<Pair<SceneType, Object>> webHistory = new Stack<Pair<SceneType, Object>>();
+	Queue<Pair<SceneType, Object>> webHistory = new LinkedList<Pair<SceneType, Object>>();
 	
 	public void initialize(Stage window) throws IOException {
 		this.window = window;
 		
-		historyWindow.initStyle(StageStyle.UNDECORATED);
-		historyWindow.setX(10);
-		historyWindow.setY(100);
+		HistoryWindow.getInstance().initialize();
 
 		FXMLLoader searchtabLoader = new FXMLLoader(getClass().getResource("searchtab.fxml"));
     	FXMLLoader homepageLoader = new FXMLLoader(getClass().getResource("homepage.fxml"));
     	FXMLLoader articleLoader = new FXMLLoader(getClass().getResource("articleview.fxml"));
     	FXMLLoader trendingLoader = new FXMLLoader(getClass().getResource("trendingtab.fxml"));
     	FXMLLoader latestLoader = new FXMLLoader(getClass().getResource("latesttab.fxml"));
-    	//FXMLLoader historyLoader = new FXMLLoader(getClass().getResource("historytab.fxml"));
         
         Scene homepage = new Scene(homepageLoader.load()); 
         Scene searchtab = new Scene(searchtabLoader.load());
@@ -81,7 +66,6 @@ public class SceneManager {
         currentSceneType = SceneType.HOMEPAGE;
         
         addHistory(webHistory);
-        box.getChildren().add(visitedWeb());
 	}
 	
 	public Scene getCurrentScene() {
@@ -114,14 +98,14 @@ public class SceneManager {
 		currentSceneType = nextSceneType;
 		
 		addHistory(webHistory);
-		box.getChildren().add(0, visitedWeb());
-		historyWindow.close();
 		
 		window.setScene(nextScene);
         window.setWidth(Screen.getPrimary().getVisualBounds().getWidth());
         window.setHeight(Screen.getPrimary().getVisualBounds().getHeight());
         
         nextPresenter.sceneSwitchInitialize();
+        
+        HistoryWindow.getInstance().historyWindow.close();
 	}
 	
 	public void moveScene(SceneType nextSceneType, Object information) {
@@ -166,73 +150,14 @@ public class SceneManager {
 			));
 	}
 	
-	private Label visitedWeb() {
-		Pair<SceneType, Object> currentWeb = webHistory.pop();
-		String webContent = new String();
-		if (currentWeb.getKey()== SceneType.ARTICLE_VIEW) {
-			ArticleData information = (ArticleData) currentWeb.getValue();
-			webContent = " : " + information.getTITLE();  
-		} else if (currentWeb.getKey()== SceneType.SEARCHTAB) {
-			webContent = " : " + currentWeb.getValue();
-		} else {
-			webContent = "";
-		}
-		
-		Label web = new Label("<> " + currentWeb.getKey() + webContent);
-		web.setCursor(Cursor.HAND);
-		web.setWrapText(true);
-		web.setOnMouseClicked(e -> {
-            moveScene(currentWeb.getKey(), currentWeb.getValue());
-        });
-		return web;
+	private void addHistory(Queue<Pair<SceneType, Object>> history) {
+		history.offer(new Pair<SceneType, Object>(
+			    currentSceneType,
+			    switch (currentSceneType) {
+			        case SEARCHTAB -> searchContent;
+			        case ARTICLE_VIEW -> selectedArticleData;
+			        default -> null; 
+			    }
+			));
 	}
-	
-	public void openHistory() {
-		BorderPane root = new BorderPane();
-        root.setPrefSize(300, 500);
-
-        StackPane centerPane = new StackPane();
-        root.setCenter(centerPane);
-
-        AnchorPane anchorPane = new AnchorPane();
-        centerPane.getChildren().add(anchorPane);
-        StackPane.setAlignment(anchorPane, javafx.geometry.Pos.CENTER);
-
-        ScrollPane scrollPane = new ScrollPane();
-        scrollPane.setPrefSize(300, 450);
-        //scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        anchorPane.getChildren().add(scrollPane);
-        AnchorPane.setTopAnchor(scrollPane, 50.0);
-
-        AnchorPane innerAnchorPane = new AnchorPane();
-        scrollPane.setContent(innerAnchorPane);
-
-        box.setPrefSize(270, 990);
-        innerAnchorPane.getChildren().add(box);
-        AnchorPane.setLeftAnchor(box, 7.0);
-        AnchorPane.setTopAnchor(box, 1.0);
-        
-        Line line = new Line(0, 0, 300, 0);
-        line.setStyle("-fx-stroke: black; -fx-stroke-width: 4;");
-        root.getChildren().add(line);
-
-        Label label = new Label("WEB BROWSING HISTORY");
-        label.setFont(new Font("System Bold", 18.0));
-        anchorPane.getChildren().add(label);
-        AnchorPane.setLeftAnchor(label, 45.0);
-        AnchorPane.setTopAnchor(label, 13.0);
-
-        Button closeButton = new Button("X");
-        closeButton.setPrefSize(25, 26);
-        closeButton.setOnAction(e -> {
-        	historyWindow.close();
-        });
-        anchorPane.getChildren().add(closeButton);
-        AnchorPane.setTopAnchor(closeButton, 5.0);
-
-        Scene scene = new Scene(root);
-        historyWindow.setScene(scene);
-        historyWindow.show();
-    }
-
 }
