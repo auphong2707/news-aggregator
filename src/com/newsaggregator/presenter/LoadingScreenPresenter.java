b90371d84e0ce98ecd3a2aca7272fb88c85acd4a
TheSyx <minhquantsy@gmail.com>
2024-05-13 00:31:50 +0700

    update loading screen

---
+++ src/com/newsaggregator/presenter/LoadingScreenPresenter.java
@@ -0,0 +1,10 @@
+package com.newsaggregator.presenter;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class LoadingScreenPresenter {
+
+    @FXML private Label loadingLabel;
+    
+}
