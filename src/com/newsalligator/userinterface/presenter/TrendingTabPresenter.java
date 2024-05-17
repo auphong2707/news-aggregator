2cbd2dcf2426109e75ccbc58a4708ca3bcfb6163
dvtiendat <111187020+dvtiendat@users.noreply.github.com>
2024-05-17 23:33:43 +0700

    TrendingTabPresenter doc init

--- src/com/newsalligator/userinterface/presenter/TrendingTabPresenter.java
+++ src/com/newsalligator/userinterface/presenter/TrendingTabPresenter.java
@@ -30,7 +30,9 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
-
+/**
+ * The {@code TrendingTabPresenter} class is a class to manage the trending tab view.
+ */
 public class TrendingTabPresenter extends Presenter {
 	@FXML private ScrollPane scrollPane;
 	
@@ -54,16 +56,31 @@ public class TrendingTabPresenter extends Presenter {
 	@FXML private Group article5;
 	@FXML private Group article6;
 	
+	/**
+	 * List of {@code ArticleData} objects representing trending articles.
+	 */
 	private List<ArticleData> trendingData;
+	/**
+	 * Page number.
+	 */
 	private int page;
+	/**
+	 * Group of articles.
+	 */
 	private Group[] articles;
 
+    /**
+     * Initializes the Trending tab view.
+     */
 	@FXML
 	void initialize() {
 		setDate();
 		articles = new Group[] {article1, article2, article3, article4, article5, article6};
 	}
 	
+    /**
+     * Sets the current date in the date label.
+     */
 	private void setDate() {
 		LocalDate currentDate = LocalDate.now();
 		int day = currentDate.getDayOfMonth();
@@ -73,6 +90,11 @@ public class TrendingTabPresenter extends Presenter {
 		dateLabel.setText(dateAbbreviation + ", " + day + "/" + month + "/" + year);
 	}
 	
+    /**
+     * Performs a search when the Enter key is pressed.
+     * 
+     * @param key the key event
+     */
 	@FXML
 	private void searchByKey(KeyEvent key) {
 		if (key.getCode() == KeyCode.ENTER) {
@@ -82,6 +104,9 @@ public class TrendingTabPresenter extends Presenter {
 		}
 	}
 	
+    /**
+     * Initiates a search when the search button is clicked.
+     */
 	@FXML
 	private void searchByButton() {
 		SearchTabCommand command = new SearchTabCommand(searchBar.getText(), "All", "All");
@@ -89,6 +114,11 @@ public class TrendingTabPresenter extends Presenter {
 		UIManager.getInstance().executeCommand(command);
 	}
 	
+    /**
+     * Switches to the next or previous page of trending articles.
+     * 
+     * @param event the action event
+     */
 	@FXML
 	private void switchPage(ActionEvent event){
 		if (event.getSource() == nextPage && page < 10) {
@@ -101,11 +131,17 @@ public class TrendingTabPresenter extends Presenter {
 		updateArticles();
 	} 
 	
+    /**
+     * Switches to the Homepage tab.
+     */
 	@FXML
 	private void switchToHomepage() {
 		UIManager.getInstance().executeCommand(new HomepageCommand());
 	}
 	
+    /**
+     * Updates the displayed trending articles based on the current page.
+     */
 	private void updateArticles() {
 		scrollPane.setVvalue(0);
 		
@@ -115,11 +151,21 @@ public class TrendingTabPresenter extends Presenter {
 		ArticleSetter.setArrayArticleViews(articles, trendingData.subList(first, last), ArticleSize.BIG);
 	}
 	
+    /**
+     * Sets the current page and updates the page label.
+     * 
+     * @param newPage the new page number
+     */
 	private void setPage(int newPage) {
 		page = newPage;
 		pageLabel.setText("Page " + page);
 	}
 	
+    /**
+     * Switches to the article view for the selected trending article.
+     * 
+     * @param event the mouse click event
+     */
 	@FXML
 	private void switchToArticle(MouseEvent event) {
 		Node clickedObject = (Node) event.getSource();
@@ -134,21 +180,35 @@ public class TrendingTabPresenter extends Presenter {
 		UIManager.getInstance().executeCommand(new ArticleTabCommand(selectedData));
     }
 	
+    /**
+     * Returns to the previous scene.
+     */
 	@FXML
 	private void returnScene() {
 		UIManager.getInstance().returnCommand();
 	}
 	
+    /**
+     * Forwards to the next scene.
+     */
 	@FXML
 	private void forwardScene() {
 		UIManager.getInstance().forwardCommand();
 	}
 	
+    /**
+     * Opens the history window.
+     */
 	@FXML 
 	private void openHistory() {
 		UIManager.getInstance().openHistoryWindow();
 	}
 	
+    /**
+     * Opens the website for the clicked link in the web browser.
+     * 
+     * @param event the mouse click event
+     */
 	@FXML
 	private void openWebsite(MouseEvent event) {
 		Label clickedObject = (Label) event.getSource();
